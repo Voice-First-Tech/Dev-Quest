0073e3e2b83a355682e8008f0922f42ddcd3194c
ian phua <ianphua@hotmail.com>
2019-01-28 19:28:25 -0500

    ian phua completed Dev Quest

---
+++ ian-phua/src/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        return this.tell('Meet Ian. He is a psychology graduate who is now a voice app developer on GoogleAssistant and Amazon Alexa.');
+    },
+});
+
+module.exports.app = app;
